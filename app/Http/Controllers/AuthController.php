798eaccf27300b88607775bd5425cd41bde7fe21
Md. Himel Ali <himelali@users.noreply.github.com>
2021-03-03 18:23:32 +0600

    Set user data with access token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,6 +103,7 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
+            'user' => $this->guard()->user(),
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => $this->guard()->factory()->getTTL() * 60
